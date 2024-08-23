7ef723c3557e624ae389ade2c4f6cb41dc54879a
unknown <emiliojoaquinortizmalich@gmail.com>
2024-08-23 14:43:58 -0300

    creacion de la rama

--- backend/src/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-import { register, login } from "../controllers/auth.controllers.js";
-import { Router } from "express";
-
-export const authRouter = Router()
-
-authRouter.post('/register', register);
-authRouter.post('/login', login);
--- backend/src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import { getAllUser, getUserById, uptadeUser, deleteUser } from "../controllers/user.controller.js";
-import { Router } from "express";
-
-export const userRouter = Router()
-
-userRouter.get('/user', getAllUser);
-userRouter.get('/user/:id', getUserById);
-userRouter.put('user/:id', uptadeUser);
-userRouter.delete('user/:id', deleteUser);
